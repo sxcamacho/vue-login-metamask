d2d102b3440b04b6303584a3ebb8612440766f7b
Sebastian Camacho <sxcamacho@gmail.com>
2022-04-29 18:00:04 -0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,34 @@ export default {
       }
     },
     doTest() {
-      alert(window ? "window OK" : "window NO");
-      var message =
-        window.ethereum && window.ethereum.isMetamask
-          ? window.ethereum.isMetamask
-          : "ERROR";
-      alert(message);
-      alert(navigator.userAgent);
+      // alert(window ? "window OK" : "window NO");
+      // var message =
+      //   window.ethereum && window.ethereum.isMetamask
+      //     ? window.ethereum.isMetamask
+      //     : "ERROR";
+      // alert(message);
+      // alert(navigator.userAgent);
+
+      if (window.ethereum) {
+        handleEthereum();
+      } else {
+        window.addEventListener("ethereum#initialized", handleEthereum, {
+          once: true,
+        });
+        // If the event is not dispatched by the end of the timeout,
+        // the user probably doesn't have MetaMask installed.
+        setTimeout(handleEthereum, 3000); // 3 seconds
+      }
+
+      function handleEthereum() {
+        const { ethereum } = window;
+        if (ethereum && ethereum.isMetaMask) {
+          alert("Ethereum successfully detected!");
+          // Access the decentralized web!
+        } else {
+          alert("Please install MetaMask!");
+        }
+      }
     },
     // async loadAccount() {
     //   if (window.ethereum && window.ethereum.selectedAddress) {
