a8bbaa6ecd63abae4a0f0189dfc40e4f48afc692
Sebastian Camacho <sxcamacho@gmail.com>
2022-04-29 15:51:46 -0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import Web3 from "web3";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      challenge: "123456",
+      address: null,
+    };
+  },
+  methods: {
+    async doLogin() {
+      if (window.ethereum) {
+        await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        this.address = window.ethereum.selectedAddress;
+      } else {
+        alert("Provider is missing!");
+      }
+    },
+    // async loadAccount() {
+    //   if (window.ethereum && window.ethereum.selectedAddress) {
+    //     this.address = window.ethereum.selectedAddress;
+    //   }
+    //   window.ethereum.on("accountsChanged", (accounts) => {
+    //     this.address = accounts[0];
+    //   });
+    // },
+    // async doSign() {
+    //   const message = Web3.utils.toUtf8(
+    //     `Welcome message: ${this.challenge}`
+    //   );
+    //   const request = {
+    //     method: "personal_sign",
+    //     params: [message, this.address],
+    //   };
+    //   const signature = await window.ethereum.request(request);
+    //   console.log("**** signature", signature);
+    // },
+  },
+  // async created() {
+  //   await this.loadAccount();
+  // },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div>
+    <h1>{{ address || "UNKNOWN" }}</h1>
+    <button @click="doLogin">LOGIN</button>
+    <!-- <div>
+      <input type="text" v-model="message" />
+      <button @click="doSign">SIGN</button>
+    </div> -->
+  </div>
+</template>
